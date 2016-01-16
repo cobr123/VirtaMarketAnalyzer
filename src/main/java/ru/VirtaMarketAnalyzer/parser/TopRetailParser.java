cb7f7ca64da25aa0cf5ca5bf66be511fbe872ea9
cobr123 <safebuster2@gmail.com>
2016-01-16 18:40:13 +0300

    парсер топа розницы

--- src/main/java/ru/VirtaMarketAnalyzer/parser/TopRetailParser.java
+++ src/main/java/ru/VirtaMarketAnalyzer/parser/TopRetailParser.java
@@ -19,30 +19,29 @@ import java.util.List;
 /**
  * Created by cobr123 on 16.01.16.
  */
-public final class UnitListParser {
-    private static final Logger logger = LoggerFactory.getLogger(UnitListParser.class);
+public final class TopRetailParser {
+    private static final Logger logger = LoggerFactory.getLogger(TopRetailParser.class);
 
     public static void main(String[] args) throws IOException {
         BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%r %d{ISO8601} [%t] %p %c %x - %m%n")));
-        final List<Shop> list = getShopList("http://virtonomica.ru/", "olga", "2085506");
+        final List<Shop> list = getShopList("http://virtonomica.ru/","olga");
         System.out.println("list.size() = " + list.size());
     }
 
-    public static List<Shop> getShopList(final String baseUrl, final String realm, final String companyId) throws IOException {
+    public static List<Shop> getShopList(final String baseUrl, final String realm) throws IOException {
         final List<Shop> shops = new ArrayList<>();
-        final String newRef = baseUrl + realm + "/main/company/view/" + companyId + "/unit_list";
+
+        final String newRef = baseUrl + realm +  "/main/company/toplist/retail";
         String nextPageUrl = newRef;
         String ref = "";
         for (; ; ) {
             final Document doc = Downloader.getDoc(nextPageUrl, ref);
-            final Elements shopLinks = doc.select("table > tbody > tr > td[class=\"info i-shop\"] > a");
-
-            logger.trace("shopLinks.size() = " + shopLinks.size());
-            for (final Element link : shopLinks) {
-                final Shop shop = ShopParser.parse(link.attr("href"));
-                if (shop.getShopProducts().size() > 0) {
-                    shops.add(shop);
-                }
+            final Elements companyLinks = doc.select("table > tbody > tr > td:nth-child(2) > span > a");
+            logger.trace("companyLinks.size() = " + companyLinks.size());
+            for (final Element link : companyLinks) {
+                final String companyId = Utils.getLastFromUrl(link.attr("href"));
+                final List<Shop> tmp = UnitListParser.getShopList(baseUrl, realm, companyId);
+                shops.addAll(tmp);
             }
 
             nextPageUrl = Utils.getNextPageHref(doc);
