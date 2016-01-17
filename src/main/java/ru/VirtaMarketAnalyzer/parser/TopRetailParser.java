a5a31fabfb45f1b60380c6a8c50d451c7eddddee
cobr123 <safebuster2@gmail.com>
2016-01-17 15:45:20 +0300

    смотрим первые 10 страниц рейтинга

--- src/main/java/ru/VirtaMarketAnalyzer/parser/TopRetailParser.java
+++ src/main/java/ru/VirtaMarketAnalyzer/parser/TopRetailParser.java
@@ -36,7 +36,7 @@ public final class TopRetailParser {
         final String newRef = baseUrl + realm + "/main/company/toplist/retail";
         String nextPageUrl = newRef;
         String ref = "";
-        for (int page = 1; page <= 5; ++page) {
+        for (int page = 1; page <= 10; ++page) {
             final Document doc = Downloader.getDoc(nextPageUrl, ref);
             final Elements companyLinks = doc.select("table > tbody > tr > td:nth-child(2) > span > a");
             logger.info("companyLinks.size() = {}", companyLinks.size());
