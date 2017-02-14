4bbcb500aff8f9ed4fc81e99c3e6e86f5fc1ffc8
safebuster <safebuster2@gmail.com>
2017-02-14 09:55:43 +0300

    добавил hashCode и equals

--- src/main/java/ru/VirtaMarketAnalyzer/data/ProductRemain.java
+++ src/main/java/ru/VirtaMarketAnalyzer/data/ProductRemain.java
@@ -1,6 +1,8 @@
 package ru.VirtaMarketAnalyzer.data;
 
 import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.util.Date;
 
@@ -9,7 +11,10 @@ import java.util.Date;
  */
 public final class ProductRemain {
     //U - UNLIMITED, L - LIMITED
-    public enum MaxOrderType {U, L}
+    public enum MaxOrderType {
+        U, L
+    }
+
     //Infinity
     @SerializedName("pi")
     final private String productID;
@@ -30,7 +35,7 @@ public final class ProductRemain {
 
     transient private Date date;
 
-    public ProductRemain(final String productID, final String unitID, final long total, final long remain, final double quality, final double price,final MaxOrderType maxOrderType, final double maxOrder) {
+    public ProductRemain(final String productID, final String unitID, final long total, final long remain, final double quality, final double price, final MaxOrderType maxOrderType, final double maxOrder) {
         this.productID = productID;
         this.unitID = unitID;
         this.total = total;
@@ -39,6 +44,39 @@ public final class ProductRemain {
         this.price = price;
         this.maxOrderType = maxOrderType;
         this.maxOrder = maxOrder;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(productID)
+                .append(unitID)
+                .append(total)
+                .append(remain)
+                .append(quality)
+                .append(price)
+                .append(maxOrderType)
+                .append(maxOrder)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj instanceof ProductRemain) {
+            final ProductRemain other = (ProductRemain) obj;
+            return new EqualsBuilder()
+                    .append(productID, other.productID)
+                    .append(unitID, other.unitID)
+                    .append(total, other.total)
+                    .append(remain, other.remain)
+                    .append(quality, other.quality)
+                    .append(price, other.price)
+                    .append(maxOrderType, other.maxOrderType)
+                    .append(maxOrder, other.maxOrder)
+                    .isEquals();
+        } else {
+            return false;
+        }
     }
 
     public String getProductID() {
