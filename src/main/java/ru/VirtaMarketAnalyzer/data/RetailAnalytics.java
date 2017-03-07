051727cc2c9dcf00e52d24984cf48dbe4a193b4f
safebuster <safebuster2@gmail.com>
2017-03-07 10:23:44 +0300

    "社区", "Спальный район"

--- src/main/java/ru/VirtaMarketAnalyzer/data/RetailAnalytics.java
+++ src/main/java/ru/VirtaMarketAnalyzer/data/RetailAnalytics.java
@@ -97,6 +97,7 @@ public final class RetailAnalytics {
                 .replace("City centre", "Центр города")
                 .replace("Центр міста", "Центр города")
                 .replace("Centro de la ciudad", "Центр города")
+                .replace("社区", "Спальный район")
                 .replace("Residential area", "Спальный район")
                 .replace("Área residencial", "Спальный район")
                 .replace("Trendy neighborhood", "Фешенебельный район")
