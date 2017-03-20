7a94093352884fbab8be8a277af26cc0321ea6a7
safebuster <safebuster2@gmail.com>
2017-03-20 10:46:58 +0300

    demography

--- src/main/java/ru/VirtaMarketAnalyzer/ml/PrepareAnalitics.java
+++ src/main/java/ru/VirtaMarketAnalyzer/ml/PrepareAnalitics.java
@@ -13,21 +13,28 @@ import java.util.stream.Collectors;
 public final class PrepareAnalitics {
     private static final Logger logger = LoggerFactory.getLogger(PrepareAnalitics.class);
 
-    public static Map<String, List<RetailAnalytics>> getRetailAnalitincsByProducts(final List<Shop> shops, final Map<String, List<TradeAtCity>> stats, final List<Product> products) {
+    public static Map<String, List<RetailAnalytics>> getRetailAnalitincsByProducts(
+            final List<Shop> shops
+            , final Map<String, List<TradeAtCity>> stats
+            , final List<Product> products
+            , final List<City> cities
+    ) {
         final Map<String, List<Shop>> shopBy = shops.parallelStream().collect(Collectors.groupingBy(Shop::getCountryRegionTownIds));
 
         final Map<String, List<RetailAnalytics>> retailAnalitincs = new HashMap<>();
 
         for (final Map.Entry<String, List<TradeAtCity>> entry : stats.entrySet()) {
             final Product product = products.stream().filter(p -> p.getId().equals(entry.getKey())).findFirst().get();
-            retailAnalitincs.put(entry.getKey(), getRetailAnalitincsByProduct(product, shopBy, entry.getValue()));
+            retailAnalitincs.put(entry.getKey(), getRetailAnalitincsByProduct(product, shopBy, entry.getValue(), cities));
         }
         return retailAnalitincs;
     }
 
     public static List<RetailAnalytics> getRetailAnalitincsByProduct(final Product product,
                                                                      final Map<String, List<Shop>> shopBy,
-                                                                     final List<TradeAtCity> stats) {
+                                                                     final List<TradeAtCity> stats,
+                                                                     final List<City> cities
+    ) {
         final Map<String, List<TradeAtCity>> statsBy = stats.parallelStream()
                 .filter(tradeAtCity -> tradeAtCity.getProductId().equals(product.getId()))
                 .collect(Collectors.groupingBy(TradeAtCity::getCountryRegionTownIds));
@@ -69,7 +76,8 @@ public final class PrepareAnalitics {
                                             tradeAtCity.getSellerCnt(),
                                             tradeAtCity.getLocalPercent(),
                                             tradeAtCity.getLocalPrice(),
-                                            tradeAtCity.getLocalQuality()
+                                            tradeAtCity.getLocalQuality(),
+                                            cities.stream().filter(c -> c.getId().equals(tradeAtCity.getTownId())).findAny().get().getDemography()
                                     )
                             );
                         } catch (final Exception e) {
