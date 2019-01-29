334c85199bc84fa733f91952733be20120e814fb
r.tabulov <rtabulov@global-system.ru>
2019-01-29 19:02:20 +0300

    feat: получение списка сервисов и специализаций через api

--- src/main/java/ru/VirtaMarketAnalyzer/parser/ServiceInitParser.java
+++ src/main/java/ru/VirtaMarketAnalyzer/parser/ServiceInitParser.java
@@ -1,11 +1,8 @@
 package ru.VirtaMarketAnalyzer.parser;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.PatternLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.VirtaMarketAnalyzer.data.Product;
@@ -17,10 +14,10 @@ import ru.VirtaMarketAnalyzer.main.Wizard;
 import ru.VirtaMarketAnalyzer.scrapper.Downloader;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * Created by cobr123 on 25.02.16.
@@ -28,84 +25,86 @@ import java.util.stream.Collectors;
 public final class ServiceInitParser {
     private static final Logger logger = LoggerFactory.getLogger(ServiceInitParser.class);
 
-    public static void main(final String[] args) throws IOException {
-        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d{ISO8601} [%t] %p %c %x - %m%n")));
-
-        final List<UnitType> list = getServiceUnitTypes(Wizard.host, "olga");
-        list.stream().forEach(opt -> {
-            logger.info(Utils.getPrettyGson(opt));
-        });
-//        final List<UnitType> list_en = getServiceUnitTypes(Wizard.host_en, "olga");
-//        list_en.stream().forEach(opt -> {
-//            logger.info(Utils.getPrettyGson(opt));
-//        });
-    }
-
     public static List<UnitType> getServiceUnitTypes(final String host, final String realm) throws IOException {
-        final Document doc = Downloader.getDoc(host + realm + "/main/globalreport/marketing/by_service");
+        final String lang = (Wizard.host.equals(host) ? "ru" : "en");
+        final String url = host + "api/" + realm + "/main/unittype/browse?lang=" + lang;
+
         final List<UnitType> list = new ArrayList<>();
+        try {
+            final Document doc = Downloader.getDoc(url, true);
+            final String json = doc.body().html();
+            final Gson gson = new Gson();
+            final Type mapType = new TypeToken<Map<String, Map<String, Object>>>() {
+            }.getType();
+            final Map<String, Map<String, Object>> mapOfUnitTypes = gson.fromJson(json, mapType);
 
-        final Elements links = doc.select("#mainContent > fieldset > span > a > img");
-        //System.out.println(list.outerHtml());
-        return links.stream()
-                .map(elem -> {
-                    try {
-                        return getServiceUnitType(host, realm, elem);
-                    } catch (IOException e) {
-                        logger.error(e.getLocalizedMessage(), e);
-                    }
-                    return null;
-                }).collect(Collectors.toList());
-    }
+            for (final String idx : mapOfUnitTypes.keySet()) {
+                final Map<String, Object> unitType = mapOfUnitTypes.get(idx);
 
-    public static UnitType getServiceUnitType(final String host, final String realm, final Element elem) throws IOException {
-        final String id = Utils.getLastBySep(elem.parent().attr("href"), "/");
-        final String caption = elem.attr("title");
-        final String imgUrl = elem.attr("src");
-        final List<UnitTypeSpec> specs = getServiceSpecs(host, realm, id);
-        //System.out.println(list.outerHtml());
-        return new UnitType(id, caption, imgUrl, specs);
-    }
+                if ("Services sector".equalsIgnoreCase(unitType.get("industry_name").toString())
+                        || "Сфера услуг".equalsIgnoreCase(unitType.get("industry_name").toString())
+                ) {
+                    final String id = unitType.get("id").toString();
+                    final String caption = unitType.get("name").toString();
+                    final String imgUrl = "/img/unit_types/" + unitType.get("symbol").toString() + ".gif";
 
-    public static List<UnitTypeSpec> getServiceSpecs(final String host, final String realm, final String id) throws IOException {
-        final Document doc = Downloader.getDoc(host + realm + "/main/industry/unit_type/info/" + id);
-        final Elements specElems = doc.select("#mainContent > table:nth-child(4) > tbody > tr > td:nth-child(1) > b");
-        return specElems.stream().map(se -> {
-            try {
-                return ServiceInitParser.getUnitTypeSpec(host, realm, se);
-            } catch (final Exception e) {
-                logger.error(e.getLocalizedMessage(), e);
+                    list.add(new UnitType(id, caption, imgUrl, getServiceSpecs(host, realm, id)));
+                }
             }
-            return null;
-        })
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+        } catch (final Exception e) {
+            logger.error(url + "&format=debug");
+            throw e;
+        }
+        return list;
     }
 
-    private static UnitTypeSpec getUnitTypeSpec(final String host, final String realm, final Element elem) throws IOException {
-        final String caption = elem.text();
-        final Element equipElem = elem.parent().parent().select(" > td:nth-child(2) > a:nth-child(1) > img").first();
-        final Product equipment = getProduct(host, realm, equipElem);
-        final Elements rawMaterialElems = elem.parent().parent().select(" > td:nth-child(3) > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > a:nth-child(1) > img");
-        final List<RawMaterial> rawMaterials = getRawMaterials(rawMaterialElems);
-        return new UnitTypeSpec(caption, equipment, rawMaterials);
+    public static List<UnitTypeSpec> getServiceSpecs(final String host, final String realm, final String unit_type_id) throws IOException {
+        final String lang = (Wizard.host.equals(host) ? "ru" : "en");
+        final String url = host + "api/" + realm + "/main/unittype/produce?id=" + unit_type_id + "&lang=" + lang;
+
+        final List<UnitTypeSpec> list = new ArrayList<>();
+        try {
+            final Document doc = Downloader.getDoc(url, true);
+            final String json = doc.body().html();
+            final Gson gson = new Gson();
+            final Type mapType = new TypeToken<Map<String, Map<String, Object>>>() {
+            }.getType();
+            final Map<String, Map<String, Object>> mapOfUnitTypes = gson.fromJson(json, mapType);
+
+            for (final String idx : mapOfUnitTypes.keySet()) {
+                final Map<String, Object> unitType = mapOfUnitTypes.get(idx);
+
+                final String caption = unitType.get("name").toString();
+                final String equipment_product_id = unitType.get("equipment_product_id").toString();
+
+                list.add(new UnitTypeSpec(caption, getProduct(host, realm, equipment_product_id), getRawMaterials(host, realm, unitType)));
+            }
+        } catch (final Exception e) {
+            logger.error(url + "&format=debug");
+            throw e;
+        }
+        return list;
     }
 
-    private static Product getProduct(final String host, final String realm, final Element equipElem) throws IOException {
-        final String id = Utils.getLastBySep(equipElem.parent().attr("href"), "/");
+    private static Product getProduct(final String host, final String realm, final String id) throws IOException {
         return ProductInitParser.getManufactureProduct(host, realm, id);
     }
 
-    private static RawMaterial getRawMaterial(final Element equipElem) {
-        final String productCategory = "";
-        final String imgUrl = equipElem.attr("src");
-        final String id = Utils.getLastBySep(equipElem.parent().attr("href"), "/");
-        final String caption = equipElem.attr("title");
-        final int quantity = Utils.toInt(equipElem.parent().parent().parent().nextElementSibling().child(0).text());
-        return new RawMaterial(productCategory, imgUrl, id, caption, quantity);
+    private static List<RawMaterial> getRawMaterials(final String host, final String realm, final Map<String, Object> unitType) throws IOException {
+        final List<RawMaterial> rawMaterials = new ArrayList<>();
+        if (unitType.get("input_product_ids") != null) {
+            final String[] input_product_ids = removeCurlyBraces(unitType.get("input_product_ids").toString()).split(",");
+            final String[] input_quantities = removeCurlyBraces(unitType.get("input_quantities").toString()).split(",");
+            for (int i = 0; i < input_product_ids.length; ++i) {
+                final Product product = getProduct(host, realm, input_product_ids[i]);
+                final double quantity = Utils.toDouble(input_quantities[i]);
+                rawMaterials.add(new RawMaterial(product, quantity));
+            }
+        }
+        return rawMaterials;
     }
 
-    private static List<RawMaterial> getRawMaterials(final Elements rawMaterialElems) {
-        return rawMaterialElems.stream().map(ServiceInitParser::getRawMaterial).collect(Collectors.toList());
+    private static String removeCurlyBraces(final String line) {
+        return line.substring(1, line.length() - 1);
     }
 }
