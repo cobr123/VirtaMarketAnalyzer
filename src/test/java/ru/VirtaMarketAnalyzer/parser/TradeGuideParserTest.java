aa4f81068c435659841f77819876532018300878
cobr123 <safebuster2@gmail.com>
2019-02-09 13:01:19 +0300

    test: простая проверка заполнения

--- src/test/java/ru/VirtaMarketAnalyzer/parser/TradeGuideParserTest.java
+++ src/test/java/ru/VirtaMarketAnalyzer/parser/TradeGuideParserTest.java
@@ -1,5 +1,8 @@
 package ru.VirtaMarketAnalyzer.parser;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
 import org.junit.jupiter.api.Test;
 import ru.VirtaMarketAnalyzer.data.*;
 import ru.VirtaMarketAnalyzer.main.Wizard;
@@ -7,6 +10,7 @@ import ru.VirtaMarketAnalyzer.main.Wizard;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by cobr123 on 08.02.2019.
@@ -15,9 +19,11 @@ public class TradeGuideParserTest {
 
     @Test
     void collectByTradeAtCitiesTest() throws Exception {
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d{ISO8601} [%t] %p %C{1} %x - %m%n")));
         final String realm = "olga";
         final List<ProductCategory> productCategories = ProductInitParser.getTradeProductCategories(Wizard.host, realm);
         final List<TradeGuide> tradeGuides = TradeGuideParser.genTradeGuide(Wizard.host, realm, productCategories.get(0));
         assertFalse(tradeGuides.isEmpty());
+        assertTrue(tradeGuides.stream().anyMatch(l -> !l.getTradeGuideProduct().isEmpty()));
     }
 }
