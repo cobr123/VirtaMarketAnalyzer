78c2737f4e630fd6340432b90dbd669c06550fed
cobr123 <safebuster2@gmail.com>
2019-02-16 15:09:54 +0300

    test: тест для одного продукта

--- src/test/java/ru/VirtaMarketAnalyzer/parser/ProductionForRetailParserTest.java
+++ src/test/java/ru/VirtaMarketAnalyzer/parser/ProductionForRetailParserTest.java
@@ -4,8 +4,11 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.PatternLayout;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.VirtaMarketAnalyzer.data.Product;
 import ru.VirtaMarketAnalyzer.data.ProductionForRetail;
+import ru.VirtaMarketAnalyzer.main.Utils;
 import ru.VirtaMarketAnalyzer.main.Wizard;
 
 import java.util.List;
@@ -13,13 +16,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductionForRetailParserTest {
+    private static final Logger logger = LoggerFactory.getLogger(ProductionForRetailParserTest.class);
 
     @Test
     void genProductionForRetailTest() throws Exception {
         BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d{ISO8601} [%t] %p %C{1} %x - %m%n")));
         final String realm = "olga";
-        final List<Product> products = ProductInitParser.getTradingProducts(Wizard.host, realm);
-        final List<ProductionForRetail> productionForRetail = ProductionForRetailParser.genProductionForRetail(Wizard.host, realm, products.get(0));
-        assertFalse(productionForRetail.isEmpty());
+        final Product product = ProductInitParser.getTradingProduct(Wizard.host, realm, "422897");
+        final List<ProductionForRetail> productionsForRetail = ProductionForRetailParser.genProductionForRetail(Wizard.host, realm, product);
+        assertFalse(productionsForRetail.isEmpty());
+        final ProductionForRetail productionForRetail = productionsForRetail.get(productionsForRetail.size() - 1);
+        logger.info(Utils.getPrettyGson(productionForRetail));
+        logger.info("https://virtonomica.ru/{}/main/globalreport/marketing?geo={}&product_id={}#by-trade-at-cities", realm, productionForRetail.getGeo(), productionForRetail.getProductID());
+        logger.info("size = {}", productionsForRetail.size());
     }
 }
