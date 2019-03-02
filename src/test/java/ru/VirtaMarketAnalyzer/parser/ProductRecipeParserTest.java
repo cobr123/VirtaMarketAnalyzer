72b2fc72c725b8b06ec1e4b3eba36562e3256102
cobr123 <safebuster2@gmail.com>
2019-03-02 14:32:04 +0300

    test: getProductRecipesTest

--- src/test/java/ru/VirtaMarketAnalyzer/parser/ProductRecipeParserTest.java
+++ src/test/java/ru/VirtaMarketAnalyzer/parser/ProductRecipeParserTest.java
@@ -25,13 +25,12 @@ class ProductRecipeParserTest {
         manufactures.add(manufacture);
         final Map<String, List<ProductRecipe>> recipes = ProductRecipeParser.getProductRecipes(host, realm, manufactures);
         //Модификатор качества для меда -20%
-        assertEquals(0, recipes.get("423151").get(0).getResultProducts().stream().filter(p -> p.getProductID().equals("423151")).collect(toList()).get(0).getQualityBonusPercent());
-        assertEquals(0, recipes.get("423151").get(1).getResultProducts().stream().filter(p -> p.getProductID().equals("423151")).collect(toList()).get(0).getQualityBonusPercent());
-        assertEquals(-20, recipes.get("423151").get(2).getResultProducts().stream().filter(p -> p.getProductID().equals("423151")).collect(toList()).get(0).getQualityBonusPercent());
-        assertEquals(3, recipes.get("423151").size());
+        assertEquals(-20, recipes.get("423153").get(0).getResultProducts().stream().filter(p -> p.getProductID().equals("423151")).collect(toList()).get(0).getQualityBonusPercent());
         //Модификатор качества для маточного молочка 0%
         assertEquals(0, recipes.get("423153").get(0).getResultProducts().stream().filter(p -> p.getProductID().equals("423153")).collect(toList()).get(0).getQualityBonusPercent());
         assertEquals(1, recipes.get("423153").size());
+        assertEquals(2, recipes.get("423153").get(0).getResultProducts().size());
         assertEquals(1, recipes.get("423153").get(0).getResultProducts().stream().filter(p -> p.getProductID().equals("423153")).collect(toList()).size());
+        assertEquals(1, recipes.get("423153").get(0).getResultProducts().stream().filter(p -> p.getProductID().equals("423151")).collect(toList()).size());
     }
 }
