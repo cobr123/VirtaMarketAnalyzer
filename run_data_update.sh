fe1c4700626a037bf0ed52f879ac57965b9f5004
safebuster2 <safebuster2@gmail.com>
2025-03-26 16:53:05 +0300

    вынес JAVA_OPTS

--- run_data_update.sh
+++ run_data_update.sh
@@ -2,4 +2,4 @@
 set -e
 
 export dayOfMonth=$(date +%d)
-java -Djava.util.Arrays.useLegacyMergeSort=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=utf-8 -cp /application.jar ru.VirtaMarketAnalyzer.main.Wizard > /logs/log_$dayOfMonth.txt 2> /logs/log_err_$dayOfMonth.txt
+java -cp /application.jar ru.VirtaMarketAnalyzer.main.Wizard > /logs/log_$dayOfMonth.txt 2> /logs/log_err_$dayOfMonth.txt
--- run_trend_update.sh
+++ run_trend_update.sh
@@ -3,4 +3,4 @@ set -e
 
 export dayOfMonth=$(date +%d)
 rm -rf /tmp/VirtaMarketAnalyzer/virtonomic*
-java -Djava.util.Arrays.useLegacyMergeSort=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=utf-8 -cp /application.jar ru.VirtaMarketAnalyzer.main.TrendUpdater > /logs/log_$dayOfMonth.txt 2> /logs/log_err_$dayOfMonth.txt
+java -cp /application.jar ru.VirtaMarketAnalyzer.main.TrendUpdater > /logs/log_$dayOfMonth.txt 2> /logs/log_err_$dayOfMonth.txt
